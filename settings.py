f98ed244264f03c9d29be6c878e1ceae0b72174e
aldyistna <aldyistna@gmail.com>
2021-07-28 11:30:04 +0700

    change host db to prod

--- settings.py
+++ settings.py
@@ -1,6 +1,11 @@
 # SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:12345@127.0.0.1:5432/db_absensi'
-SQLALCHEMY_DATABASE_URI = 'postgresql://fhbehzymylyjjg' \
-                          ':e85fcc4162f15b660f632ac5e9b43d70abd7442ddb993f5ae07f8aaa39bc5a30@' \
-                          'ec2-54-236-137-173.compute-1.amazonaws.com:5432/d4p0adqt357310'
+# dev
+# SQLALCHEMY_DATABASE_URI = 'postgresql://fhbehzymylyjjg' \
+#                           ':e85fcc4162f15b660f632ac5e9b43d70abd7442ddb993f5ae07f8aaa39bc5a30@' \
+#                           'ec2-54-236-137-173.compute-1.amazonaws.com:5432/d4p0adqt357310'
+# prod
+SQLALCHEMY_DATABASE_URI = 'postgresql://yjnsmtoxfaaqhb' \
+                          ':e193b5517dcb06fbbea73cc4d6740ca76ec07c96d99807fe1bf160dfc1f33319@' \
+                          'ec2-52-0-67-144.compute-1.amazonaws.com:5432/d2vegcf4ud5fub'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 BUNDLE_ERRORS = True
